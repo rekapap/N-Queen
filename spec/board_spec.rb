8d06602389a00a88f7555885a6b4844aca7877f6
Reka <rekapap28@gmail.com>
2019-02-01 10:31:11 +0000

    refactors board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,13 +4,15 @@ describe Board do
   subject(:board) { described_class.new(size) }
   subject(:board_2) { described_class.new(size_2) }
   let(:size) { 4 }
-  let(:size_2) {  8 }
+  let(:size_2) { 8 }
 
-  it 'has a size' do
-    expect(board.size).to eq(size)
-  end
+  describe '#size' do
+    it 'has a size' do
+      expect(board.size).to eq(size)
+    end
 
-  it 'can have different sizes' do
-    expect(board_2.size).to eq(size_2)
+    it 'can have different sizes' do
+      expect(board_2.size).to eq(size_2)
+    end
   end
 end
