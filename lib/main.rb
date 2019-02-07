e85aa5df6720b41aedc712959f570491a34860bb
Reka <rekapap28@gmail.com>
2019-02-07 20:43:11 +0000

    adds unhappy path to main

--- lib/main.rb
+++ lib/main.rb
@@ -5,11 +5,15 @@ require_relative './game'
 # Main Program
 class Main
   BOARD_SIZE_QUESTION = 'What board size do you want?'.freeze
+  WRONG_BOARD_SIZE = 'Select an integer above 4!'.freeze
+
   attr_reader :game
 
   def setup
     puts(BOARD_SIZE_QUESTION)
     board_size = gets.chomp.to_i
+    raise WRONG_BOARD_SIZE if board_size < 4
+
     @game = Game.new(board_size)
   end
 end
